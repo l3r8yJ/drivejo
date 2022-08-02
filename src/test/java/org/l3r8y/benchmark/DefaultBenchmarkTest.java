229b02e620d63a53c60b962e30a14b641d780185
Ivan <clicker.heroes.acg@gmail.com>
2022-08-03 02:56:25 +0300

    empty test, main removed, test dep added

---
+++ src/test/java/org/l3r8y/benchmark/DefaultBenchmarkTest.java
@@ -0,0 +1,25 @@
+package org.l3r8y.benchmark;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class DefaultBenchmarkTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void log() {
+    }
+
+    @Test
+    void bench() {
+    }
+}
